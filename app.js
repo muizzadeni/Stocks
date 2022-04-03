329501116917a9bd4a6eec747fd15b31dd5ee2f6
Muizz Adeni <muizzadeni@hotmail.com>
2022-04-03 08:21:09 +0500

    completed challenge 2-6

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const path = require('path')
 const stocks = require('./stocks')
+var cors = require('cors') /*Added cors library*/
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'static')),cors())
 
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+app.get('/stocks', async (req, res,next) => {
+  try { /*To catch an error*/
+    const stockSymbols = await stocks.getStocks()
+    //const stockSymbols = await [] // Challenge 6 To display error message due to empty array
+    res.send({ stockSymbols })
+  }catch(err){
+    next(err)
+  }
+
 })
 
-app.get('/stocks/:symbol', async (req, res) => {
+app.get('/stocks/:symbol', async (req, res,next) => {
+  try {
   const { params: { symbol } } = req
   const data = await stocks.getStockPoints(symbol, new Date())
   res.send(data)
+  }catch(err){
+    next(err)
+  }
 })
 
 app.listen(3000, () => console.log('Server is running!'))
